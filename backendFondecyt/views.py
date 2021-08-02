8a5a40038e0a542e2224c65ecb782b568d9a961d
marceloansaldi <ansaldi.marcelo@gmail.com>
2021-08-02 18:04:46 -0400

    proposito analisis

--- backendFondecyt/views.py
+++ backendFondecyt/views.py
@@ -11,61 +11,50 @@ import mammoth
 import json
 
 class FileUploadView(APIView):
-    parser_classes = (MultiPartParser, FormParser)
+  parser_classes = (MultiPartParser, FormParser)
 
-    def post(self, request, format=None):
-        # tipo_analisis = request.POST['tipo_analisis']
-        up_file = request.FILES['file']
-        #  llamadas 
-        destination = open('backendFondecyt/Docs/' + up_file.name, 'wb+')
-        for chunk in up_file.chunks():
-            destination.write(chunk)
-        destination.close()  # File should be closed only after all chuns are added
+  def post(self, request, format=None):
+    # tipo_analisis = request.POST['tipo_analisis']
+    up_file = request.FILES['file']
+    #  llamadas 
+    destination = open('backendFondecyt/Docs/' + up_file.name, 'wb+')
+    for chunk in up_file.chunks():
+      destination.write(chunk)
+    destination.close()  # File should be closed only after all chuns are added
 
-        with open('backendFondecyt/Docs/' + up_file.name, "rb") as docx_file:
-            result = mammoth.convert_to_html(docx_file)
-            rawText = mammoth.extract_raw_text(docx_file)
-            html = result.value  # The generated HTML
+    with open('backendFondecyt/Docs/' + up_file.name, "rb") as docx_file:
+      result = mammoth.convert_to_html(docx_file)
+      rawText = mammoth.extract_raw_text(docx_file)
+      html = result.value  # The generated HTML
 
-            f = open("backendFondecyt/Docs/output.html", "wb")
-            f.write(html.encode('utf8'))
-            f.close()
-            data = {
-              'html': html.encode('utf8'),
-              'passive_voice': self.PostRedilegra(rawText, html, "passive_voice"),
-              'statistics': self.PostRedilegra(rawText, html, "statistics"),
-              'oraciones': self.PostRedilegra(rawText, html, "oraciones"),
-              'micro_paragraphs': self.PostRedilegra(rawText, html,  "micro_paragraphs"),
-              'gerunds': self.PostRedilegra(rawText, html, "gerunds"),
-              'fs_person': self.PostRedilegra(rawText, html, "fs_person"),
-              'analisis_concordancia': self.PostRedilegra(rawText, html, "analisis_concordancia"),
-              'proposito': self.PostRedilegra(rawText, html, "proposito"),
-              'conectores': self.PostRedilegra(rawText, html, "conectores"),
-              'sentence_complexity': self.PostRedilegra(rawText, html, "sentence_complexity"),
-              'lecturabilidad_parrafo': self.PostRedilegra(rawText, html, "lecturabilidad_parrafo"),
-            }
-        return Response(data, status.HTTP_201_CREATED)
-
-    def PostRedilegra(self, rawtext, html, endpoint):
-      payload = {
-        'texto': rawtext.value,
-        'html': html,
+      f = open("backendFondecyt/Docs/output.html", "wb")
+      f.write(html.encode('utf8'))
+      f.close()
+      data = {
+        'html': html.encode('utf8'),
+        'passive_voice': self.PostRedilegra(rawText, html, "passive_voice"),
+        'statistics': self.PostRedilegra(rawText, html, "statistics"),
+        'oraciones': self.PostRedilegra(rawText, html, "oraciones"),
+        'micro_paragraphs': self.PostRedilegra(rawText, html,  "micro_paragraphs"),
+        'gerunds': self.PostRedilegra(rawText, html, "gerunds"),
+        'fs_person': self.PostRedilegra(rawText, html, "fs_person"),
+        'analisis_concordancia': self.PostRedilegra(rawText, html, "analisis_concordancia"),
+        'proposito': self.PostRedilegra(rawText, html, "proposito"),
+        'conectores': self.PostRedilegra(rawText, html, "conectores"),
+        'sentence_complexity': self.PostRedilegra(rawText, html, "sentence_complexity"),
+        'lecturabilidad_parrafo': self.PostRedilegra(rawText, html, "lecturabilidad_parrafo"),
       }
-      url = 'http://redilegra.com/'+endpoint
-      x = requests.post(url, data=payload)
-      # print(x.text.encode('utf8'))
-      return json.loads(x.text.encode('utf8'))
+    return Response(data, status.HTTP_201_CREATED)
 
-    # def PostRedilegra(self, rawtext, html, function):
-    #     payload = {
-    #         'texto': rawtext.value,
-    #         'funcion': function,
-    #         'html': html,
-    #     }
-    #     url = 'http://redilegra.com/palabra'
-    #     x = requests.post(url, data=payload)
-    #     return x.text.encode('utf8')
-
+  def PostRedilegra(self, rawtext, html, endpoint):
+    payload = {
+      'texto': rawtext.value,
+      'html': html,
+    }
+    url = 'http://redilegra.com/'+endpoint
+    x = requests.post(url, data=payload)
+    # print(x.text.encode('utf8'))
+    return json.loads(x.text.encode('utf8'))
 
 class Concordancia(APIView):
   parser_classes = (MultiPartParser, FormParser)
@@ -89,25 +78,27 @@ class PostTextRedilegra(APIView):
     print(request)
     html = request.POST['html']
     text = request.POST['text']
+    macromovida = request.POST['macromovida']
     data = {
-              'html': html.encode('utf8'),
-              'passive_voice': self.PostRedilegra(text, html, "passive_voice"),
-              'statistics': self.PostRedilegra(text, html, "statistics"),
-              'oraciones': self.PostRedilegra(text, html, "oraciones"),
-              'micro_paragraphs': self.PostRedilegra(text, html,  "micro_paragraphs"),
-              'gerunds': self.PostRedilegra(text, html, "gerunds"),
-              'fs_person': self.PostRedilegra(text, html, "fs_person"),
-              'sentence_complexity': self.PostRedilegra(text, html, "sentence_complexity"),
-              'analisis_concordancia': self.PostRedilegra(text, html, "analisis_concordancia"),
-              'proposito': self.PostRedilegra(text, html, "proposito"),
-              'conectores': self.PostRedilegra(text, html, "conectores"),
-            }
+      'html': html.encode('utf8'),
+      'proposito': self.PostRedilegra(text, html, "proposito", macromovida),
+      # 'statistics': self.PostRedilegra(text, html, "statistics"),
+      # 'oraciones': self.PostRedilegra(text, html, "oraciones"),
+      # 'micro_paragraphs': self.PostRedilegra(text, html,  "micro_paragraphs"),
+      # 'gerunds': self.PostRedilegra(text, html, "gerunds"),
+      # 'fs_person': self.PostRedilegra(text, html, "fs_person"),
+      # 'sentence_complexity': self.PostRedilegra(text, html, "sentence_complexity"),
+      # 'analisis_concordancia': self.PostRedilegra(text, html, "analisis_concordancia"),
+      # 'proposito': self.PostRedilegra(text, html, "proposito"),
+      # 'conectores': self.PostRedilegra(text, html, "conectores"),
+    }
     return Response(data, status.HTTP_201_CREATED)
 
-  def PostRedilegra(self, rawtext, html, endpoint):
+  def PostRedilegra(self, rawtext, html, endpoint, macromovida):
     payload = {
       'texto': rawtext,
       'html': html,
+      'proposito': macromovida
     }
     url = 'http://redilegra.com/'+endpoint
     x = requests.post(url, data=payload)
