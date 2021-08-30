7b6f262397b2cf16efb1fe46d19b7cc72912826f
marceloansaldi <ansaldi.marcelo@gmail.com>
2021-08-30 02:33:06 -0400

    txt soporte añadido

--- backendFondecyt/views.py
+++ backendFondecyt/views.py
@@ -16,39 +16,46 @@ class FileUploadView(APIView):
   def post(self, request, format=None):
     # tipo_analisis = request.POST['tipo_analisis']
     up_file = request.FILES['file']
-    #  llamadas 
     destination = open('backendFondecyt/Docs/' + up_file.name, 'wb+')
     for chunk in up_file.chunks():
       destination.write(chunk)
     destination.close()  # File should be closed only after all chuns are added
-
-    with open('backendFondecyt/Docs/' + up_file.name, "rb") as docx_file:
-      result = mammoth.convert_to_html(docx_file)
-      rawText = mammoth.extract_raw_text(docx_file)
-      html = result.value  # The generated HTML
-
-      f = open("backendFondecyt/Docs/output.html", "wb")
-      f.write(html.encode('utf8'))
-      f.close()
-      data = {
-        'html': html.encode('utf8'),
-        'passive_voice': self.PostRedilegra(rawText, html, "passive_voice"),
-        'statistics': self.PostRedilegra(rawText, html, "statistics"),
-        'oraciones': self.PostRedilegra(rawText, html, "oraciones"),
-        'micro_paragraphs': self.PostRedilegra(rawText, html,  "micro_paragraphs"),
-        'gerunds': self.PostRedilegra(rawText, html, "gerunds"),
-        'fs_person': self.PostRedilegra(rawText, html, "fs_person"),
-        'analisis_concordancia': self.PostRedilegra(rawText, html, "analisis_concordancia"),
-        'proposito': self.PostRedilegra(rawText, html, "proposito"),
-        'conectores': self.PostRedilegra(rawText, html, "conectores"),
-        'sentence_complexity': self.PostRedilegra(rawText, html, "sentence_complexity"),
-        'lecturabilidad_parrafo': self.PostRedilegra(rawText, html, "lecturabilidad_parrafo"),
-      }
+    if (up_file.name.split(".")[1] == "docx"):
+      with open('backendFondecyt/Docs/' + up_file.name, "rb") as docx_file:
+        rawText = mammoth.extract_raw_text(docx_file)
+        rawText = rawText.value
+        result = mammoth.convert_to_html(docx_file)
+        html = result.value
+    else:
+        txt_file = open('backendFondecyt/Docs/' + up_file.name, "r", encoding="utf-8")
+        rawText = txt_file.read()
+        txt_file.close()
+        txt_file = open('backendFondecyt/Docs/' + up_file.name, "r", encoding="utf-8")
+        html = ""
+        for line in txt_file:
+          stripped_line = line.rstrip()
+          if (stripped_line.strip() != ""): 
+            html += "<p>" + line + "</p>"
+        txt_file.close()
+    data = {
+      'html': html.encode('utf8'),
+      'passive_voice': self.PostRedilegra(rawText, html, "passive_voice"),
+      'statistics': self.PostRedilegra(rawText, html, "statistics"),
+      'oraciones': self.PostRedilegra(rawText, html, "oraciones"),
+      'micro_paragraphs': self.PostRedilegra(rawText, html,  "micro_paragraphs"),
+      'gerunds': self.PostRedilegra(rawText, html, "gerunds"),
+      'fs_person': self.PostRedilegra(rawText, html, "fs_person"),
+      'analisis_concordancia': self.PostRedilegra(rawText, html, "analisis_concordancia"),
+      'proposito': self.PostRedilegra(rawText, html, "proposito"),
+      'conectores': self.PostRedilegra(rawText, html, "conectores"),
+      'sentence_complexity': self.PostRedilegra(rawText, html, "sentence_complexity"),
+      'lecturabilidad_parrafo': self.PostRedilegra(rawText, html, "lecturabilidad_parrafo"),
+    }
     return Response(data, status.HTTP_201_CREATED)
 
   def PostRedilegra(self, rawtext, html, endpoint):
     payload = {
-      'texto': rawtext.value,
+      'texto': rawtext,
       'html': html,
     }
     url = 'http://redilegra.com/'+endpoint
